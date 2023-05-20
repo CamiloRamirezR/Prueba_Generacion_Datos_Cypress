9c06d9c54ac67dee3e18b80025541bd063c2d24c
julianpuentesuribe <w.puentes@uniandes.edu.co>
2023-05-19 20:13:43 -0500

    Se carga escenario P050

---
+++ cypress/e2e/a_priori/P050.cy.js
@@ -0,0 +1,53 @@
+import TagSection from "../../support/elements/tagSection";
+//import AdminMenu from "../support/elements/adminMenu";
+import AdminMenu from "../../support/elements/adminMenu";
+import Site from "../../support/elements/site";
+import jsonData from "./data/P050.json";
+
+const tagSection = new TagSection();
+const adminMenu = new AdminMenu();
+const site = new Site();
+
+describe("Crear Tag con 191 carácteres en el Slug", () => {
+  it("Crear Tag con 191 carácteres en el Slug", () => {
+    /* 
+    -------------
+      GIVEN
+    -------------
+    */
+
+    // Autentica un usuario que puede crear tags
+    cy.login();
+    // Va a la pestaña Tags
+    adminMenu.tagTab.click();
+    cy.wait(1000);
+
+    /* 
+    -------------
+      WHEN
+    -------------
+    */
+
+    // Información crea la tag    
+    // Puedes acceder a los valores individualmente
+    const title = jsonData.title;
+    const slug = jsonData.title;
+    const description = jsonData.title;
+    // intenta publicar el tag
+    tagSection.createTagMockarooData(title,slug, description) ;
+    tagSection.saveTag.click();
+    /* 
+    -------------
+      THEN
+    -------------
+    */
+   
+    // Verifica que el tag aparezca en el listado de tags
+    adminMenu.tagTab.click();
+    cy.url().then(basAeUrl => {
+      cy.log(basAeUrl);
+      cy.visit(basAeUrl + ('/') + title.toLowerCase());
+    });
+    cy.wait(2000);
+  });
+});
